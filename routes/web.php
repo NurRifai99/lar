56024a4e85485b83e947e56bb80f8c139c770af5
NurRifai99 <nurrahmanrifai3@gmail.com>
2024-10-08 08:38:51 +0700

    buat regis

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RegistrasiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,4 +24,7 @@ Route::get(uri: '/', action: [LoginController::class, 'login'])->name(name: 'log
 Route::post(uri: 'actionlogin', action: [LoginController::class, 'actionlogin'])->name(name: 'actionlogin');
 
 Route::get(uri: 'home', action: [HomeController::class, 'index'])->name(name: 'home')->middleware(middleware: 'auth');
-Route::get(uri: 'actionlogout', action: [LoginController::class, 'actionlogout'])->name(name: 'actionlogout');
+Route::get(uri: 'actionlogout', action: [LoginController::class, 'actionlogout'])->name(name: 'actionlogout'); 
+
+Route::get('register', [RegistrasiController::class, 'index'])->name('register');
+Route::post('register/action', [RegistrasiController::class, 'actionregister'])->name('actionregister');
